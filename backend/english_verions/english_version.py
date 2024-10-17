dc4402c1c6baec216f4a93a7235fe302d2f6de29
Amine-HADJEMI <hadjemiamine@outlook.fr>
2024-10-17 11:15:12 +0200

    fix: apis"

--- backend/english_verions/english_version.py
+++ backend/english_verions/english_version.py
@@ -1,21 +1,30 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
 import os
+from flask_cors import CORS
 
-from flask_cors import CORS  
 app = Flask(__name__)
-
 classifier = pipeline("text-classification", model="unitary/toxic-bert")
 
-CORS(app, resources={r"/*": {"origins": "https://my-express-app-1058119729143.us-central1.run.app"}}) 
+# Autoriser les requêtes provenant de ton domaine Express uniquement
+CORS(app, resources={r"/*": {"origins": "https://my-express-app-1058119729143.us-central1.run.app"}})
 
-@app.route('/detect_insult', methods=['POST'])
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'https://my-express-app-1058119729143.us-central1.run.app')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    return response
+
+@app.route('/detect_insult', methods=['POST', 'OPTIONS'])
 def detect_insult():
+    if request.method == 'OPTIONS':
+        return jsonify({'status': 'OK'}), 200
+
     try:
         data = request.get_json()
         text = data['text']
 
-    
         result = classifier(text)
 
         return jsonify(result), 200
